00e2feead76118321ccca7b736971ed3c1213f15
nkoliset <nkoliset@in.ibm.com>
2020-10-12 17:02:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Header } from './components/Header';
+import './App.css';
+import Balance from './components/Balance';
+import TransactionList from './components/TransactionList';
+import AddTransaction from './components/AddTransaction';
+import { GlobalProvider } from './context/GlobalState';
+import { IncomeExpense } from './components/IncomeExpense';
+
+
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpense />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
